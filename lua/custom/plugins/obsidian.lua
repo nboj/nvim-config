893c0969130cb60180099a7f2744abb5a260b81f
nboj <60493251+nboj@users.noreply.github.com>
2025-09-19 15:02:22 -0400

    u

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,3 +1,35 @@
+-- Open the currently viewed file in Obsidian
+local function url_encode(s)
+  return (s:gsub("([^%w%-_%.~])", function(c)
+    return string.format("%%%02X", string.byte(c))
+  end))
+end
+
+local function open_in_obsidian()
+  local file = vim.fn.expand("%:p")
+  if file == "" or vim.fn.filereadable(file) == 0 then
+    vim.notify("No readable file in this buffer.", vim.log.levels.WARN)
+    return
+  end
+
+  -- Obsidian supports either ?path=... or ?vault=...&file=...
+  -- Using path works if the file lives inside any of your vaults.
+  local url = "obsidian://open?path=" .. url_encode(file)
+
+  local cmd
+  if vim.fn.has("macunix") == 1 then
+    cmd = { "open", url }
+  elseif vim.fn.has("win32") == 1 then
+    cmd = { "cmd.exe", "/c", "start", "", url }
+  else
+    cmd = { "xdg-open", url }
+  end
+
+  vim.fn.jobstart(cmd, { detach = true })
+end
+
+-- your mapping (I'd suggest <leader>o instead of '<leader><o>')
+vim.keymap.set("n", "<leader>o", open_in_obsidian, { desc = "Open in Obsidian" })
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
