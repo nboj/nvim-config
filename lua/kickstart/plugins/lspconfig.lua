f86ddc077595815cfb12bf722b89904b38d79dd4
nboj <60493251+nboj@users.noreply.github.com>
2026-01-12 15:20:51 -0500

    u

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -231,6 +231,7 @@ return {
         lua_ls = {
         },
         clangd = {},
+        r_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
